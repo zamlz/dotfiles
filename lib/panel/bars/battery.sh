e38cdc3306309ae5b99dbd069abb9dd57e88565e
Amlesh Sivanantham <samlesh@gmail.com>
2018-05-16 18:42:33 -0700

    Made the battery script color reflect the status of the battery

--- lib/panel/bars/battery.sh
+++ lib/panel/bars/battery.sh
@@ -8,14 +8,27 @@
 battery() {
     batloc="/sys/class/power_supply/"
     batlist=$(ls $batloc | grep "BAT")
-    out="[power%{F${CYAN}}"
+    out="[power "
 
     for bat in $batlist; do
         cap="$(cat ${batloc}${bat}/capacity)"
         stat="$(cat ${batloc}${bat}/status)"
-        out="${out} ${bat}: ${cap}% (${stat})"
+
+        if [ "$stat" = "Charging" ]; then
+            out="${out} %{F${GREEN}}${bat}: ${cap}% %{F-}"
+
+        elif [ $cap -le 5 ]; then
+            out="${out} %{F${RED}}${bat}: ${cap}% %{F-}"
+
+        elif [ $cap -le 25 ]; then
+            out="${out} %{F${YELLOW}}${bat}: ${cap}% %{F-}"
+
+        else
+            out="${out} %{F${CYAN}}${bat}: ${cap}% %{F-}"
+        fi
+
     done
-    out="${out}%{F-}]"
+    out="${out}]"
 
     echo -n $out
 }
