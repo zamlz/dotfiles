c48b3e0bd76cdbd6b37793cddcdb66a7444ff5ee
Amlesh Sivanantham <samlesh@gmail.com>
2018-05-18 12:36:05 -0700

    Fixed some spacing issues with the battery bar

--- lib/panel/bars/battery.sh
+++ lib/panel/bars/battery.sh
@@ -8,23 +8,23 @@
 battery() {
     batloc="/sys/class/power_supply/"
     batlist=$(ls $batloc | grep "BAT")
-    out="[power "
+    out="[power"
 
     for bat in $batlist; do
         cap="$(cat ${batloc}${bat}/capacity)"
         stat="$(cat ${batloc}${bat}/status)"
 
         if [ "$stat" = "Charging" ]; then
-            out="${out} %{F${GREEN}}${bat}: ${cap}% %{F-}"
+            out="${out} %{F${GREEN}}${bat}: ${cap}%%{F-}"
 
         elif [ $cap -le 5 ]; then
-            out="${out} %{F${RED}}${bat}: ${cap}% %{F-}"
+            out="${out} %{F${RED}}${bat}: ${cap}%%{F-}"
 
         elif [ $cap -le 25 ]; then
-            out="${out} %{F${YELLOW}}${bat}: ${cap}% %{F-}"
+            out="${out} %{F${YELLOW}}${bat}: ${cap}%%{F-}"
 
         else
-            out="${out} %{F${CYAN}}${bat}: ${cap}% %{F-}"
+            out="${out} %{F${CYAN}}${bat}: ${cap}%%{F-}"
         fi
 
     done
