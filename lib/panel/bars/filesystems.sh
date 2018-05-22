ed1ec340d78ed111bdd089271564f4ad7f7a95e0
Amlesh Sivanantham <samlesh@gmail.com>
2018-05-22 16:21:04 -0700

    Fixed panel stderr print issues, and also fixed locker

--- lib/panel/bars/filesystems.sh
+++ lib/panel/bars/filesystems.sh
@@ -7,22 +7,29 @@
 
 filesystems() {
 
-    root=$(lsblk -rpo MOUNTPOINT,NAME | grep '/ ' | awk '{print $2}')
-    rootsize=$(df -h | grep ${root} | awk '{print $2}')
-    rootused=$(df -h | grep ${root} | awk '{print $3}')
+    out="[filesystems %{F${YELLOW}}"
     
     boot=$(lsblk -rpo MOUNTPOINT,NAME | grep '/boot' | awk '{print $2}')
-    bootsize=$(df -h | grep ${boot} | awk '{print $2}')
-    bootused=$(df -h | grep ${boot} | awk '{print $3}')
+    if [ -n "$boot" ]; then
+        bootsize=$(df -h | grep ${boot} | awk '{print $2}')
+        bootused=$(df -h | grep ${boot} | awk '{print $3}')
+        out="${out} $boot: $bootused/$bootsize"
+    fi
+    
+    root=$(lsblk -rpo MOUNTPOINT,NAME | grep '/ ' | awk '{print $2}')
+    if [ -n "$root" ]; then
+        rootsize=$(df -h | grep ${root} | awk '{print $2}')
+        rootused=$(df -h | grep ${root} | awk '{print $3}')
+        out="${out} $root: $rootused/$rootsize"
+    fi
     
     home=$(lsblk -rpo MOUNTPOINT,NAME | grep '/home' | awk '{print $2}')
-    homesize=$(df -h | grep ${home} | awk '{print $2}')
-    homeused=$(df -h | grep ${home} | awk '{print $3}')
+    if [ -n "$home" ]; then
+        homesize=$(df -h | grep ${home} | awk '{print $2}')
+        homeused=$(df -h | grep ${home} | awk '{print $3}')
+        out="${out} $home: $homeused/$homesize"
+    fi
 
-    out="[filesystems %{F${YELLOW}}"
-    if [ -n "$boot" ]; then out="${out} $boot: $bootused/$bootsize"; fi
-    if [ -n "$root" ]; then out="${out} $root: $rootused/$rootsize"; fi
-    if [ -n "$home" ]; then out="${out} $home: $homeused/$homesize"; fi
     out="${out}%{F-}]"
 
     echo -n $out
