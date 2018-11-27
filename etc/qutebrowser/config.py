451b27777717f59a2589d5f99ad752a80cd4dd3f
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2018-11-26 19:05:00 -0800

    Qutebrowser uses dina font

--- etc/qutebrowser/config.py
+++ etc/qutebrowser/config.py
@@ -851,7 +851,7 @@ c.downloads.location.directory = "~/tmp"
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
 ## Type: Font
-# c.fonts.monospace = '"xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+c.fonts.monospace = ' Dina, "xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 
 ## Font used for prompts.
 ## Type: Font
