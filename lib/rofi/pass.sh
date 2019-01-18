5919e57f5805005d05ad5adf6527fdc349a08aa3
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2019-01-18 15:45:12 -0800

    Fixed rofi pass syntax

--- lib/rofi/pass.sh
+++ lib/rofi/pass.sh
@@ -4,4 +4,6 @@ pass_name=$(find ~/.password-store -type f | grep "gpg" | \
     sed -e "s|$HOME\/\.password-store\/||g" -e "s|\.gpg||g" | \
     rofi -dmenu -i -p "pass" )
 
-[[ $pass_name != "" ]] && pass -c $pass_name
+if [ $pass_name != "" ]; then
+    pass -c $pass_name
+fi
