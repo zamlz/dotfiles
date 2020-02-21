09de8b355182b3c49a8010692e5b58d6767e66c6
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2020-02-21 08:12:19 -0800

    Added xset dpms toggles for dock and undocked mode

--- etc/herbstluftwm/thinkpad_dock_toggle.sh
+++ etc/herbstluftwm/thinkpad_dock_toggle.sh
@@ -11,7 +11,8 @@ LAPTOP="DP-2"
 MONITOR_STATE=$(xrandr | grep "$MONITOR connected")
 
 if [ -n "$MONITOR_STATE" ]; then
-
+    
+    xset -dpms
     xrandr --output DP-1.1 --mode 1920x1080 --pos 3000x656 --rotate normal \
            --output DP-1.2 --primary --mode 1920x1080 --pos 0x544 --rotate normal \
            --output DP-1.3 --mode 1920x1080 --pos 1920x0 --rotate left \
@@ -22,7 +23,7 @@ if [ -n "$MONITOR_STATE" ]; then
            #--output DP-2 --mode 1920x1080 --pos 0x544 --rotate normal \
 else
 
-
+    xset +dpms
     xrandr --output DP-1.1 --off \
            --output DP-1.2 --off \
            --output DP-1.3 --off \
