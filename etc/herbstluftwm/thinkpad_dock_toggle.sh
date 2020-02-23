903c337e3155b40fa0ffb51a9b45a9957cd3d55a
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2020-02-23 09:53:03 -0800

    disentangled display profiles

--- etc/herbstluftwm/thinkpad_dock_toggle.sh
+++ etc/herbstluftwm/thinkpad_dock_toggle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export DISPLAY=":0"
-export XAUTHORITY=$HOME/.Xauthority
+# export DISPLAY=":0"
+# export XAUTHORITY=$HOME/.Xauthority
 
 # Need to only check the first one...
 # DP-{1.1,1.2,1.3}
@@ -11,24 +11,7 @@ LAPTOP="DP-2"
 MONITOR_STATE=$(xrandr | grep "$MONITOR connected")
 
 if [ -n "$MONITOR_STATE" ]; then
-    
-    xset -dpms
-    xrandr --output DP-1.1 --mode 1920x1080 --pos 3000x656 --rotate normal \
-           --output DP-1.2 --primary --mode 1920x1080 --pos 0x544 --rotate normal \
-           --output DP-1.3 --mode 1920x1080 --pos 1920x0 --rotate left \
-           --output HDMI-0 --off \
-           --output DP-2 --off \
-           --output DP-1 --off \
-           --output DP-0 --off
-           #--output DP-2 --mode 1920x1080 --pos 0x544 --rotate normal \
+    $HOME/lib/profiles/display/thinkpad_dock
 else
-
-    xset +dpms
-    xrandr --output DP-1.1 --off \
-           --output DP-1.2 --off \
-           --output DP-1.3 --off \
-           --output HDMI-0 --off \
-           --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal \
-           --output DP-1 --off --output DP-0 --off
-
+    $HOME/lib/profiles/display/default
 fi
