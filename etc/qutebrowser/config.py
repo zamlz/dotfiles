1db8fd661fb70bbd088c329a74df1f3c1b72c5bc
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2020-07-26 17:01:55 -0700

    Added some more config stuff to qutebrowser

--- etc/qutebrowser/config.py
+++ etc/qutebrowser/config.py
@@ -93,7 +93,398 @@ c.downloads.location.directory = "~/tmp"
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
 ## Type: Font
-c.fonts.default_family = ['Dina', "xos4 Terminus", 'Terminus, Monospace',
+c.fonts.default_family = ['Iosevka Term', 'Dina', "xos4 Terminus", 'Terminus, Monospace',
         "DejaVu Sans Mono", 'Monaco', "Bitstream Vera Sans Mono",
         "Andale Mono", "Courier New", 'Courier', "Liberation Mono",
         'monospace', 'Fixed', 'Consolas', 'Terminal']
+
+## Default font size to use. Whenever "default_size" is used in a font
+## setting, it's replaced with the size listed here. Valid values are
+## either a float value with a "pt" suffix, or an integer value with a
+## "px" suffix.
+## Type: String
+c.fonts.default_size = '12pt'
+
+## Which algorithm to use for modifying how colors are rendered with
+## darkmode.
+## Type: String
+## Valid values:
+##   - lightness-cielab: Modify colors by converting them to CIELAB color space and inverting the L value.
+##   - lightness-hsl: Modify colors by converting them to the HSL color space and inverting the lightness (i.e. the "L" in HSL).
+##   - brightness-rgb: Modify colors by subtracting each of r, g, and b from their maximum value.
+c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+
+## Contrast for dark mode. This only has an effect when
+## `colors.webpage.darkmode.algorithm` is set to `lightness-hsl` or
+## `brightness-rgb`.
+## Type: Float
+c.colors.webpage.darkmode.contrast = 0.0
+
+## Render all web contents using a dark theme. Example configurations
+## from Chromium's `chrome://flags`:  - "With simple HSL/CIELAB/RGB-based
+## inversion": Set   `colors.webpage.darkmode.algorithm` accordingly.  -
+## "With selective image inversion": Set
+## `colors.webpage.darkmode.policy.images` to `smart`.  - "With selective
+## inversion of non-image elements": Set
+## `colors.webpage.darkmode.threshold.text` to 150 and
+## `colors.webpage.darkmode.threshold.background` to 205.  - "With
+## selective inversion of everything": Combines the two variants   above.
+## Type: Bool
+c.colors.webpage.darkmode.enabled = True
+
+## Render all colors as grayscale. This only has an effect when
+## `colors.webpage.darkmode.algorithm` is set to `lightness-hsl` or
+## `brightness-rgb`.
+## Type: Bool
+c.colors.webpage.darkmode.grayscale.all = False
+
+## Desaturation factor for images in dark mode. If set to 0, images are
+## left as-is. If set to 1, images are completely grayscale. Values
+## between 0 and 1 desaturate the colors accordingly.
+## Type: Float
+c.colors.webpage.darkmode.grayscale.images = 0.0
+
+## Which images to apply dark mode to. WARNING: On Qt 5.15.0, this
+## setting can cause frequent renderer process crashes due to a
+## https://codereview.qt-project.org/c/qt/qtwebengine-
+## chromium/+/304211[bug in Qt].
+## Type: String
+## Valid values:
+##   - always: Apply dark mode filter to all images.
+##   - never: Never apply dark mode filter to any images.
+##   - smart: Apply dark mode based on image content.
+c.colors.webpage.darkmode.policy.images = 'never'
+
+## Which pages to apply dark mode to.
+## Type: String
+## Valid values:
+##   - always: Apply dark mode filter to all frames, regardless of content.
+##   - smart: Apply dark mode filter to frames based on background color.
+c.colors.webpage.darkmode.policy.page = 'smart'
+
+## Threshold for inverting background elements with dark mode. Background
+## elements with brightness above this threshold will be inverted, and
+## below it will be left as in the original, non-dark-mode page. Set to
+## 256 to never invert the color or to 0 to always invert it. Note: This
+## behavior is the opposite of `colors.webpage.darkmode.threshold.text`!
+## Type: Int
+c.colors.webpage.darkmode.threshold.background = 100
+
+## Threshold for inverting text with dark mode. Text colors with
+## brightness below this threshold will be inverted, and above it will be
+## left as in the original, non-dark-mode page. Set to 256 to always
+## invert text color or to 0 to never invert text color.
+## Type: Int
+c.colors.webpage.darkmode.threshold.text = 156
+
+## Where to show the downloaded files.
+## Type: VerticalPosition
+## Valid values:
+##   - top
+##   - bottom
+c.downloads.position = 'bottom'
+
+## Bindings for normal mode
+config.bind("'", 'enter-mode jump_mark')
+config.bind('+', 'zoom-in')
+config.bind('-', 'zoom-out')
+config.bind('.', 'repeat-command')
+config.bind('/', 'set-cmd-text /')
+config.bind(':', 'set-cmd-text :')
+config.bind(';I', 'hint images tab')
+config.bind(';O', 'hint links fill :open -t -r {hint-url}')
+config.bind(';R', 'hint --rapid links window')
+config.bind(';Y', 'hint links yank-primary')
+config.bind(';b', 'hint all tab-bg')
+config.bind(';d', 'hint links download')
+config.bind(';f', 'hint all tab-fg')
+config.bind(';h', 'hint all hover')
+config.bind(';i', 'hint images')
+config.bind(';o', 'hint links fill :open {hint-url}')
+config.bind(';r', 'hint --rapid links tab-bg')
+config.bind(';t', 'hint inputs')
+config.bind(';y', 'hint links yank')
+config.bind('<Alt-1>', 'tab-focus 1')
+config.bind('<Alt-2>', 'tab-focus 2')
+config.bind('<Alt-3>', 'tab-focus 3')
+config.bind('<Alt-4>', 'tab-focus 4')
+config.bind('<Alt-5>', 'tab-focus 5')
+config.bind('<Alt-6>', 'tab-focus 6')
+config.bind('<Alt-7>', 'tab-focus 7')
+config.bind('<Alt-8>', 'tab-focus 8')
+config.bind('<Alt-9>', 'tab-focus -1')
+config.bind('<Alt-m>', 'tab-mute')
+config.bind('<Ctrl-A>', 'navigate increment')
+config.bind('<Ctrl-Alt-p>', 'print')
+config.bind('<Ctrl-B>', 'scroll-page 0 -1')
+config.bind('<Ctrl-D>', 'scroll-page 0 0.5')
+config.bind('<Ctrl-F5>', 'reload -f')
+config.bind('<Ctrl-F>', 'scroll-page 0 1')
+config.bind('<Ctrl-N>', 'open -w')
+config.bind('<Ctrl-PgDown>', 'tab-next')
+config.bind('<Ctrl-PgUp>', 'tab-prev')
+config.bind('<Ctrl-Q>', 'quit')
+config.bind('<Ctrl-Return>', 'follow-selected -t')
+config.bind('<Ctrl-Shift-N>', 'open -p')
+config.bind('<Ctrl-Shift-T>', 'undo')
+config.bind('<Ctrl-Shift-Tab>', 'nop')
+config.bind('<Ctrl-Shift-W>', 'close')
+config.bind('<Ctrl-T>', 'open -t')
+config.bind('<Ctrl-Tab>', 'tab-focus last')
+config.bind('<Ctrl-U>', 'scroll-page 0 -0.5')
+config.bind('<Ctrl-V>', 'enter-mode passthrough')
+config.bind('<Ctrl-W>', 'tab-close')
+config.bind('<Ctrl-X>', 'navigate decrement')
+config.bind('<Ctrl-^>', 'tab-focus last')
+config.bind('<Ctrl-h>', 'home')
+config.bind('<Ctrl-p>', 'tab-pin')
+config.bind('<Ctrl-s>', 'stop')
+config.bind('<Escape>', 'clear-keychain ;; search ;; fullscreen --leave')
+config.bind('<F11>', 'fullscreen')
+config.bind('<F5>', 'reload')
+config.bind('<Return>', 'follow-selected')
+config.bind('<back>', 'back')
+config.bind('<forward>', 'forward')
+config.bind('=', 'zoom')
+config.bind('?', 'set-cmd-text ?')
+config.bind('@', 'run-macro')
+config.bind('B', 'set-cmd-text -s :quickmark-load -t')
+config.bind('D', 'tab-close -o')
+config.bind('F', 'hint all tab')
+config.bind('G', 'scroll-to-perc')
+config.bind('H', 'back')
+config.bind('J', 'tab-next')
+config.bind('K', 'tab-prev')
+config.bind('L', 'forward')
+config.bind('M', 'bookmark-add')
+config.bind('N', 'search-prev')
+config.bind('O', 'set-cmd-text -s :open -t')
+config.bind('PP', 'open -t -- {primary}')
+config.bind('Pp', 'open -t -- {clipboard}')
+config.bind('R', 'reload -f')
+config.bind('Sb', 'open qute://bookmarks#bookmarks')
+config.bind('Sh', 'open qute://history')
+config.bind('Sq', 'open qute://bookmarks')
+config.bind('Ss', 'open qute://settings')
+config.bind('T', 'tab-focus')
+config.bind('V', 'enter-mode caret ;; toggle-selection --line')
+config.bind('ZQ', 'quit')
+config.bind('ZZ', 'quit --save')
+config.bind('[[', 'navigate prev')
+config.bind(']]', 'navigate next')
+config.bind('`', 'enter-mode set_mark')
+config.bind('ad', 'download-cancel')
+config.bind('b', 'set-cmd-text -s :quickmark-load')
+config.bind('cd', 'download-clear')
+config.bind('co', 'tab-only')
+config.bind('d', 'tab-close')
+config.bind('f', 'hint')
+config.bind('g$', 'tab-focus -1')
+config.bind('g0', 'tab-focus 1')
+config.bind('gB', 'set-cmd-text -s :bookmark-load -t')
+config.bind('gC', 'tab-clone')
+config.bind('gD', 'tab-give')
+config.bind('gO', 'set-cmd-text :open -t -r {url:pretty}')
+config.bind('gU', 'navigate up -t')
+config.bind('g^', 'tab-focus 1')
+config.bind('ga', 'open -t')
+config.bind('gb', 'set-cmd-text -s :bookmark-load')
+config.bind('gd', 'download')
+config.bind('gf', 'view-source')
+config.bind('gg', 'scroll-to-perc 0')
+config.bind('gi', 'hint inputs --first')
+config.bind('gl', 'tab-move -')
+config.bind('gm', 'tab-move')
+config.bind('go', 'set-cmd-text :open {url:pretty}')
+config.bind('gr', 'tab-move +')
+config.bind('gt', 'set-cmd-text -s :buffer')
+config.bind('gu', 'navigate up')
+config.bind('h', 'scroll left')
+config.bind('i', 'enter-mode insert')
+config.bind('j', 'scroll down')
+config.bind('k', 'scroll up')
+config.bind('l', 'scroll right')
+config.bind('m', 'quickmark-save')
+config.bind('n', 'search-next')
+config.bind('o', 'set-cmd-text -s :open')
+config.bind('pP', 'open -- {primary}')
+config.bind('pp', 'open -- {clipboard}')
+config.bind('q', 'record-macro')
+config.bind('r', 'reload')
+config.bind('sf', 'save')
+config.bind('sk', 'set-cmd-text -s :bind')
+config.bind('sl', 'set-cmd-text -s :set -t')
+config.bind('ss', 'set-cmd-text -s :set')
+config.bind('tCH', 'config-cycle -p -u *://*.{url:host}/* content.cookies.accept all no-3rdparty never ;; reload')
+config.bind('tCh', 'config-cycle -p -u *://{url:host}/* content.cookies.accept all no-3rdparty never ;; reload')
+config.bind('tCu', 'config-cycle -p -u {url} content.cookies.accept all no-3rdparty never ;; reload')
+config.bind('tIH', 'config-cycle -p -u *://*.{url:host}/* content.images ;; reload')
+config.bind('tIh', 'config-cycle -p -u *://{url:host}/* content.images ;; reload')
+config.bind('tIu', 'config-cycle -p -u {url} content.images ;; reload')
+config.bind('tPH', 'config-cycle -p -u *://*.{url:host}/* content.plugins ;; reload')
+config.bind('tPh', 'config-cycle -p -u *://{url:host}/* content.plugins ;; reload')
+config.bind('tPu', 'config-cycle -p -u {url} content.plugins ;; reload')
+config.bind('tSH', 'config-cycle -p -u *://*.{url:host}/* content.javascript.enabled ;; reload')
+config.bind('tSh', 'config-cycle -p -u *://{url:host}/* content.javascript.enabled ;; reload')
+config.bind('tSu', 'config-cycle -p -u {url} content.javascript.enabled ;; reload')
+config.bind('tcH', 'config-cycle -p -t -u *://*.{url:host}/* content.cookies.accept all no-3rdparty never ;; reload')
+config.bind('tch', 'config-cycle -p -t -u *://{url:host}/* content.cookies.accept all no-3rdparty never ;; reload')
+config.bind('tcu', 'config-cycle -p -t -u {url} content.cookies.accept all no-3rdparty never ;; reload')
+config.bind('th', 'back -t')
+config.bind('tiH', 'config-cycle -p -t -u *://*.{url:host}/* content.images ;; reload')
+config.bind('tih', 'config-cycle -p -t -u *://{url:host}/* content.images ;; reload')
+config.bind('tiu', 'config-cycle -p -t -u {url} content.images ;; reload')
+config.bind('tl', 'forward -t')
+config.bind('tpH', 'config-cycle -p -t -u *://*.{url:host}/* content.plugins ;; reload')
+config.bind('tph', 'config-cycle -p -t -u *://{url:host}/* content.plugins ;; reload')
+config.bind('tpu', 'config-cycle -p -t -u {url} content.plugins ;; reload')
+config.bind('tsH', 'config-cycle -p -t -u *://*.{url:host}/* content.javascript.enabled ;; reload')
+config.bind('tsh', 'config-cycle -p -t -u *://{url:host}/* content.javascript.enabled ;; reload')
+config.bind('tsu', 'config-cycle -p -t -u {url} content.javascript.enabled ;; reload')
+config.bind('u', 'undo')
+config.bind('v', 'enter-mode caret')
+config.bind('wB', 'set-cmd-text -s :bookmark-load -w')
+config.bind('wIh', 'devtools left')
+config.bind('wIj', 'devtools bottom')
+config.bind('wIk', 'devtools top')
+config.bind('wIl', 'devtools right')
+config.bind('wIw', 'devtools window')
+config.bind('wO', 'set-cmd-text :open -w {url:pretty}')
+config.bind('wP', 'open -w -- {primary}')
+config.bind('wb', 'set-cmd-text -s :quickmark-load -w')
+config.bind('wf', 'hint all window')
+config.bind('wh', 'back -w')
+config.bind('wi', 'devtools')
+config.bind('wl', 'forward -w')
+config.bind('wo', 'set-cmd-text -s :open -w')
+config.bind('wp', 'open -w -- {clipboard}')
+config.bind('xO', 'set-cmd-text :open -b -r {url:pretty}')
+config.bind('xo', 'set-cmd-text -s :open -b')
+config.bind('yD', 'yank domain -s')
+config.bind('yM', 'yank inline [{title}]({url}) -s')
+config.bind('yP', 'yank pretty-url -s')
+config.bind('yT', 'yank title -s')
+config.bind('yY', 'yank -s')
+config.bind('yd', 'yank domain')
+config.bind('ym', 'yank inline [{title}]({url})')
+config.bind('yp', 'yank pretty-url')
+config.bind('yt', 'yank title')
+config.bind('yy', 'yank')
+config.bind('{{', 'navigate prev -t')
+config.bind('}}', 'navigate next -t')
+
+## Bindings for caret mode
+config.bind('$', 'move-to-end-of-line', mode='caret')
+config.bind('0', 'move-to-start-of-line', mode='caret')
+config.bind('<Ctrl-Space>', 'drop-selection', mode='caret')
+config.bind('<Escape>', 'leave-mode', mode='caret')
+config.bind('<Return>', 'yank selection', mode='caret')
+config.bind('<Space>', 'toggle-selection', mode='caret')
+config.bind('G', 'move-to-end-of-document', mode='caret')
+config.bind('H', 'scroll left', mode='caret')
+config.bind('J', 'scroll down', mode='caret')
+config.bind('K', 'scroll up', mode='caret')
+config.bind('L', 'scroll right', mode='caret')
+config.bind('V', 'toggle-selection --line', mode='caret')
+config.bind('Y', 'yank selection -s', mode='caret')
+config.bind('[', 'move-to-start-of-prev-block', mode='caret')
+config.bind(']', 'move-to-start-of-next-block', mode='caret')
+config.bind('b', 'move-to-prev-word', mode='caret')
+config.bind('c', 'enter-mode normal', mode='caret')
+config.bind('e', 'move-to-end-of-word', mode='caret')
+config.bind('gg', 'move-to-start-of-document', mode='caret')
+config.bind('h', 'move-to-prev-char', mode='caret')
+config.bind('j', 'move-to-next-line', mode='caret')
+config.bind('k', 'move-to-prev-line', mode='caret')
+config.bind('l', 'move-to-next-char', mode='caret')
+config.bind('o', 'reverse-selection', mode='caret')
+config.bind('v', 'toggle-selection', mode='caret')
+config.bind('w', 'move-to-next-word', mode='caret')
+config.bind('y', 'yank selection', mode='caret')
+config.bind('{', 'move-to-end-of-prev-block', mode='caret')
+config.bind('}', 'move-to-end-of-next-block', mode='caret')
+
+## Bindings for command mode
+config.bind('<Alt-B>', 'rl-backward-word', mode='command')
+config.bind('<Alt-Backspace>', 'rl-backward-kill-word', mode='command')
+config.bind('<Alt-D>', 'rl-kill-word', mode='command')
+config.bind('<Alt-F>', 'rl-forward-word', mode='command')
+config.bind('<Ctrl-?>', 'rl-delete-char', mode='command')
+config.bind('<Ctrl-A>', 'rl-beginning-of-line', mode='command')
+config.bind('<Ctrl-B>', 'rl-backward-char', mode='command')
+config.bind('<Ctrl-C>', 'completion-item-yank', mode='command')
+config.bind('<Ctrl-D>', 'completion-item-del', mode='command')
+config.bind('<Ctrl-E>', 'rl-end-of-line', mode='command')
+config.bind('<Ctrl-F>', 'rl-forward-char', mode='command')
+config.bind('<Ctrl-H>', 'rl-backward-delete-char', mode='command')
+config.bind('<Ctrl-K>', 'rl-kill-line', mode='command')
+config.bind('<Ctrl-N>', 'command-history-next', mode='command')
+config.bind('<Ctrl-P>', 'command-history-prev', mode='command')
+config.bind('<Ctrl-Return>', 'command-accept --rapid', mode='command')
+config.bind('<Ctrl-Shift-C>', 'completion-item-yank --sel', mode='command')
+config.bind('<Ctrl-Shift-Tab>', 'completion-item-focus prev-category', mode='command')
+config.bind('<Ctrl-Tab>', 'completion-item-focus next-category', mode='command')
+config.bind('<Ctrl-U>', 'rl-unix-line-discard', mode='command')
+config.bind('<Ctrl-W>', 'rl-unix-word-rubout', mode='command')
+config.bind('<Ctrl-Y>', 'rl-yank', mode='command')
+config.bind('<Down>', 'completion-item-focus --history next', mode='command')
+config.bind('<Escape>', 'leave-mode', mode='command')
+config.bind('<Return>', 'command-accept', mode='command')
+config.bind('<Shift-Delete>', 'completion-item-del', mode='command')
+config.bind('<Shift-Tab>', 'completion-item-focus prev', mode='command')
+config.bind('<Tab>', 'completion-item-focus next', mode='command')
+config.bind('<Up>', 'completion-item-focus --history prev', mode='command')
+
+## Bindings for hint mode
+config.bind('<Ctrl-B>', 'hint all tab-bg', mode='hint')
+config.bind('<Ctrl-F>', 'hint links', mode='hint')
+config.bind('<Ctrl-R>', 'hint --rapid links tab-bg', mode='hint')
+config.bind('<Escape>', 'leave-mode', mode='hint')
+config.bind('<Return>', 'follow-hint', mode='hint')
+
+## Bindings for insert mode
+config.bind('<Ctrl-E>', 'open-editor', mode='insert')
+config.bind('<Escape>', 'leave-mode', mode='insert')
+config.bind('<Shift-Ins>', 'insert-text -- {primary}', mode='insert')
+
+## Bindings for passthrough mode
+config.bind('<Shift-Escape>', 'leave-mode', mode='passthrough')
+
+## Bindings for prompt mode
+config.bind('<Alt-B>', 'rl-backward-word', mode='prompt')
+config.bind('<Alt-Backspace>', 'rl-backward-kill-word', mode='prompt')
+config.bind('<Alt-D>', 'rl-kill-word', mode='prompt')
+config.bind('<Alt-F>', 'rl-forward-word', mode='prompt')
+config.bind('<Alt-Shift-Y>', 'prompt-yank --sel', mode='prompt')
+config.bind('<Alt-Y>', 'prompt-yank', mode='prompt')
+config.bind('<Ctrl-?>', 'rl-delete-char', mode='prompt')
+config.bind('<Ctrl-A>', 'rl-beginning-of-line', mode='prompt')
+config.bind('<Ctrl-B>', 'rl-backward-char', mode='prompt')
+config.bind('<Ctrl-E>', 'rl-end-of-line', mode='prompt')
+config.bind('<Ctrl-F>', 'rl-forward-char', mode='prompt')
+config.bind('<Ctrl-H>', 'rl-backward-delete-char', mode='prompt')
+config.bind('<Ctrl-K>', 'rl-kill-line', mode='prompt')
+config.bind('<Ctrl-P>', 'prompt-open-download --pdfjs', mode='prompt')
+config.bind('<Ctrl-U>', 'rl-unix-line-discard', mode='prompt')
+config.bind('<Ctrl-W>', 'rl-unix-word-rubout', mode='prompt')
+config.bind('<Ctrl-X>', 'prompt-open-download', mode='prompt')
+config.bind('<Ctrl-Y>', 'rl-yank', mode='prompt')
+config.bind('<Down>', 'prompt-item-focus next', mode='prompt')
+config.bind('<Escape>', 'leave-mode', mode='prompt')
+config.bind('<Return>', 'prompt-accept', mode='prompt')
+config.bind('<Shift-Tab>', 'prompt-item-focus prev', mode='prompt')
+config.bind('<Tab>', 'prompt-item-focus next', mode='prompt')
+config.bind('<Up>', 'prompt-item-focus prev', mode='prompt')
+
+## Bindings for register mode
+config.bind('<Escape>', 'leave-mode', mode='register')
+
+## Bindings for yesno mode
+config.bind('<Alt-Shift-Y>', 'prompt-yank --sel', mode='yesno')
+config.bind('<Alt-Y>', 'prompt-yank', mode='yesno')
+config.bind('<Escape>', 'leave-mode', mode='yesno')
+config.bind('<Return>', 'prompt-accept', mode='yesno')
+config.bind('N', 'prompt-accept --save no', mode='yesno')
+config.bind('Y', 'prompt-accept --save yes', mode='yesno')
+config.bind('n', 'prompt-accept no', mode='yesno')
+config.bind('y', 'prompt-accept yes', mode='yesno')
