85313a0a2ebdf8eb0a0c3f14ac82888cfb536e02
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-01-11 18:19:48 -0800

    better headings for org mode

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -91,3 +91,12 @@
 
 (setq org-log-done t)
 (setq org-agenda-files (list "~/org"))
+(setq org-hide-emphasis-markers t)
+
+(font-lock-add-keywords 'org-mode
+  '(("^ *\\([-]\\) "
+  (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
