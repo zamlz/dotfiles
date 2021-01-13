503c0cc2f9a3823576544c1e4cbc47321d89009c
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-01-12 21:10:07 -0800

    Massively reworked my emacs config

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -1,42 +1,24 @@
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+;;    _______   ____  __   ______                         
+;;   / ____/ | / / / / /  / ____/___ ___  ____ ___________
+;;  / / __/  |/ / / / /  / __/ / __ `__ \/ __ `/ ___/ ___/
+;; / /_/ / /|  / /_/ /  / /___/ / / / / / /_/ / /__(__  ) 
+;; \____/_/ |_/\____/  /_____/_/ /_/ /_/\__,_/\___/____/  
+;; ----------------------------------------------------------------------------
+;; My Personal GNU Emacs Configuration
+;; ----------------------------------------------------------------------------
 
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-
-(menu-bar-mode -1)            ; Disable the menu bar
-
-;; Set up the visible bell
-(setq visible-bell t)
-
-;; Set custom font (type face)
-;;(set-face-attribute 'default nil :font "xos4 Terminus" :height 110)
-(set-face-attribute 'default nil :font "Iosevka Term" :height 110)
-
-;; Set a custom theme
-(load-theme 'wombat)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Setup line numbers
-(column-number-mode)
-(global-display-line-numbers-mode t)
-
-;; Disable line numbers in some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+;; ----------------------------------------------------------------------------
+;; PACKAGE MANAGEMENT
+;; ----------------------------------------------------------------------------
 
 ;; Initialize package sources
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org"   . "https://orgmode.org/elpa/")
+                         ("elpa"  . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -49,27 +31,60 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; ----------------------------------------------------------------------------
+;; APPEARANCE SETTINGS
+;; ----------------------------------------------------------------------------
+
+;; no startup screen! (maybe I want a custom one though)
+(setq inhibit-startup-message t)
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Cleanup the sidebars
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)          ; Disable the menu bar
+
+;; Setup line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers in some modes
+(dolist (mode '(term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Set custom font (type face)
+;;(set-face-attribute 'default nil :font "xos4 Terminus" :height 110)
+(set-face-attribute 'default nil :font "Iosevka Term" :height 110)
+
 ;; to install the fonts, you must also run this command
 ;;    M-x all-the-icons-install-fonts
 (use-package all-the-icons)
+
+;; Modeline needs the icons above to be installed
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
-;; Install custom doom themes
-;;(use-package doom-themes
-;;  :config (setq doom-themes-enable-bold t
-;;                doom-themes-enable-italic t
-;;                doom-gruvbox-dark-variant "hard")
-;;          (load-theme 'doom-gruvbox t))
+;; Setup the gruvbox theme lol
 (use-package gruvbox-theme
   :init (load-theme 'gruvbox t))
-;;(set-background-color "black")
 
 ;; rainbow delimiters for programming parenthesis
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+
+;; ----------------------------------------------------------------------------
+;; CUSTOM KEYBINDINGS
+;; ----------------------------------------------------------------------------
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Useful package to show what keybindings are available
 ;; under each prefix
@@ -83,7 +98,9 @@
   :ensure t
   :init (evil-mode 1))
 
-;; All org mode related settings
+;; ----------------------------------------------------------------------------
+;; ORG MODE SETTINGS
+;; ----------------------------------------------------------------------------
 
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -100,3 +117,12 @@
 (use-package org-bullets
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;; ----------------------------------------------------------------------------
+;; MISC CONFIGS 
+;; ----------------------------------------------------------------------------
+
+(use-package magit)
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
