9c96f49381efd2208529bf1aa372abb6a76f398b
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-01-22 22:22:29 -0800

    Added autocomplete command completion in emacs via ivy

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -102,6 +102,9 @@
 (use-package gruvbox-theme
   :init (load-theme 'gruvbox-dark-hard t))
 
+;; Also set the background to black
+(set-background-color "black")
+
 ;; rainbow delimiters for programming parenthesis
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -156,6 +159,41 @@
 ;; ----------------------------------------------------------------------------
 ;; AUTOCOMPLETION ENGINE AND DOCUMENTATION REDUX
 ;; ----------------------------------------------------------------------------
+
+(use-package counsel
+  :after ivy
+  :config (counsel-mode))
+
+(use-package ivy
+  :defer 0.1
+  :diminish
+  :bind (("C-x B" . ivy-switch-buffer-other-window)
+	 :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  :config (ivy-mode))
+
+(use-package ivy-rich
+  :after ivy
+  :config
+  (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
+  (ivy-rich-mode))
+
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)))
 
 ;; ----------------------------------------------------------------------------
 ;; GIT AND PROJECT CONFIGURATION
