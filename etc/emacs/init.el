7478b4adec2e450352ccbbbc55a75935ceac0bf8
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-01-26 23:56:42 -0800

    Updated emacs config, added org-roam

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -561,6 +561,18 @@
 
 (add-to-list 'org-modules 'org-habit t)
 
+(use-package org-roam
+  :ensure t
+  :hook (after-init . org-roam-mode)
+  :custom (org-roam-directory "~/usr/notes/")
+  :bind (:map org-roam-mode-map
+         (("C-c n l" . org-roam)
+         ("C-c n f" . org-roam-find-file)
+         ("C-c n g" . org-roam-graph))
+         :map org-mode-map
+         (("C-c n i" . org-roam-insert))
+         (("C-c n I" . org-roam-insert-immediate))))
+
 (use-package vterm
   :ensure t)
 
