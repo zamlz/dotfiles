5fd7748371ccb63d325873437599d3d7418b1861
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-01-28 00:42:12 -0800

    Updated a lot

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -214,13 +214,37 @@
 ;; (use-package powerline
 ;;   :init (powerline-default-theme))
 
+;; (use-package smart-mode-line
+;;    :config
+;;    (sml/setup))
+
+(add-to-list 'custom-theme-load-path "~/etc/emacs/themes/")
+
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
 ;;(use-package gruvbox-theme
 ;;  :init (load-theme 'gruvbox-dark-hard t))
 ;;(set-background-color "black")
 
-(use-package spacemacs-theme
-  :defer t
-  :init (load-theme 'spacemacs-dark t))
+;; (use-package spacemacs-theme
+;;   :defer t
+;;   :init (load-theme 'spacemacs-dark t))
+
+;; (load-theme 'xresources t)
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
