fe96492bb0ea8a0eb994b4240e7ea0a78d226735
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-02-28 23:44:42 -0800

    trying out helm

--- etc/init.el
+++ etc/init.el
@@ -251,50 +251,56 @@
 (zamlz/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
 
-(use-package ivy
-  :defer 0.1
-  :diminish
-  :bind (("C-x B" . ivy-switch-buffer-other-window)
-     :map ivy-minibuffer-map
-     ("TAB" . ivy-alt-done)
-     ("C-l" . ivy-alt-done)
-     ("C-j" . ivy-next-line)
-     ("C-k" . ivy-previous-line)
-     :map ivy-switch-buffer-map
-     ("C-k" . ivy-previous-line)
-     ("C-l" . ivy-done)
-     ("C-d" . ivy-switch-buffer-kill)
-     :map ivy-reverse-i-search-map
-     ("C-k" . ivy-previous-line)
-     ("C-d" . ivy-reverse-i-search-kill))
-  :custom
-  (ivy-count-format "(%d/%d) ")
-  (ivy-use-virtual-buffers t)
-  :config (ivy-mode))
+;; (use-package ivy
+;;   :defer 0.1
+;;   :diminish
+;;   :bind (("C-x B" . ivy-switch-buffer-other-window)
+;;      :map ivy-minibuffer-map
+;;      ("TAB" . ivy-alt-done)
+;;      ("C-l" . ivy-alt-done)
+;;      ("C-j" . ivy-next-line)
+;;      ("C-k" . ivy-previous-line)
+;;      :map ivy-switch-buffer-map
+;;      ("C-k" . ivy-previous-line)
+;;      ("C-l" . ivy-done)
+;;      ("C-d" . ivy-switch-buffer-kill)
+;;      :map ivy-reverse-i-search-map
+;;      ("C-k" . ivy-previous-line)
+;;      ("C-d" . ivy-reverse-i-search-kill))
+;;   :custom
+;;   (ivy-count-format "(%d/%d) ")
+;;   (ivy-use-virtual-buffers t)
+;;   :config (ivy-mode))
 
-(use-package counsel
-  :after ivy
-  :bind (("M-x" . counsel-M-x)
-     ("C-x b" . counsel-switch-buffer)
-     ("C-x C-f" . counsel-find-file)
-     :map minibuffer-local-map
-     ("C-r" . 'counsel-minibuffer-history))
-  :config (counsel-mode))
+;; (use-package counsel
+;;   :after ivy
+;;   :bind (("M-x" . counsel-M-x)
+;;      ("C-x b" . counsel-switch-buffer)
+;;      ("C-x C-f" . counsel-find-file)
+;;      :map minibuffer-local-map
+;;      ("C-r" . 'counsel-minibuffer-history))
+;;   :config (counsel-mode))
 
-;; TODO: Figure out what swiper is lol
-(use-package swiper
-  :after ivy
-  :bind (("C-s" . swiper)))
+;; ;; TODO: Figure out what swiper is lol
+;; (use-package swiper
+;;   :after ivy
+;;   :bind (("C-s" . swiper)))
 
-;; Adds nice icons to the ivy rich buffer
-(use-package all-the-icons-ivy-rich
-  :after counsel-projectile
-  :init (all-the-icons-ivy-rich-mode 1))
+;; ;; Adds nice icons to the ivy rich buffer
+;; (use-package all-the-icons-ivy-rich
+;;   :after counsel-projectile
+;;   :init (all-the-icons-ivy-rich-mode 1))
 
-;; Actually install ivy rich
-(use-package ivy-rich
-  :after (ivy all-the-icons-ivy-rich)
-  :init (ivy-rich-mode 1))
+;; ;; Actually install ivy rich
+;; (use-package ivy-rich
+;;   :after (ivy all-the-icons-ivy-rich)
+;;   :init (ivy-rich-mode 1))
+
+(use-package helm
+  :bind (("M-x" . helm-M-x))
+  :config
+  (require 'helm-config)
+  (helm-mode 1))
 
 (use-package all-the-icons)
 
