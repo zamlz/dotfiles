d4da7a9f49a8d6af4a207a6e76f8f5bba162a28f
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-11-26 11:47:36 -0800

    Added gnupg files

---
+++ etc/gnupg/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# GnuPG Setup Script
+# ------------------
+
+. $HOME/lib/shell/logging && eval "$(get_logger etc.gnupg.setup)"
+
+logger "Setting up GnuPG"
+
+CONFIG_SOURCE=$HOME/etc/gnupg
+CONFIG_TARGET=$HOME/.gnupg
+
+if [ ! -d "$CONFIG_TARGET" ]; then
+    logger "Making directory $CONFIG_TARGET"
+    mkdir $CONFIG_TARGET
+fi
+
+logger "Creating symlink for gpg.conf"
+ln -s $CONFIG_SOURCE/gpg.conf $CONFIG_TARGET/gpg.conf
+
+logger "Creating symlink for gpg-agent.conf"
+ln -s $CONFIG_SOURCE/gpg-agent.conf $CONFIG_TARGET/gpg-agent.conf
