830477805a8013bd63fe0d14680bc79b1f2755ba
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-11-27 22:13:03 -0800

    Added rofi

---
+++ etc/portage/package.use/x11-libs.cairo
@@ -0,0 +1,5 @@
+# required by x11-libs/pango-1.48.10-r1::gentoo
+# required by gnome-base/librsvg-2.50.7::gentoo
+# required by x11-misc/rofi-1.7.0-r1::gentoo
+# required by rofi (argument)
+>=x11-libs/cairo-1.16.0-r5 X
---
+++ etc/rofi/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# GnuPG Setup Script
+# ------------------
+
+. $HOME/lib/shell/logging && eval "$(get_logger $0)"
+
+logger "Setting up Rofi"
+
+CONFIG_SOURCE=$HOME/etc/rofi
+CONFIG_TARGET=$HOME/.config/rofi
+
+if [ ! -d "$CONFIG_TARGET" ]; then
+    logger "Making directory $CONFIG_TARGET"
+    mkdir -p $CONFIG_TARGET
+fi
+
+logger "Creating symlink for config.rasi"
+ln -s $CONFIG_SOURCE/config.rasi $CONFIG_TARGET/config.rasi
+
+logger "Creating symlink for default-theme.rasi"
+ln -s $CONFIG_SOURCE/default-theme.rasi $CONFIG_TARGET/default-theme.rasi
