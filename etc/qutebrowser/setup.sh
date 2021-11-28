06f8766c6c02e6618c37bdca998ad609552c270e
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-11-27 22:22:41 -0800

    Added some qutebrowser configuration

---
+++ etc/qutebrowser/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# GnuPG Setup Script
+# ------------------
+
+. $HOME/lib/shell/logging && eval "$(get_logger $0)"
+
+logger "Setting up Qutebrowser"
+
+CONFIG_SOURCE=$HOME/etc/qutebrowser
+CONFIG_TARGET=$HOME/.config/qutebrowser
+
+if [ ! -d "$CONFIG_TARGET" ]; then
+    logger "Making directory $CONFIG_TARGET"
+    mkdir -p $CONFIG_TARGET
+fi
+
+logger "Creating symlink for config.py"
+ln -s $CONFIG_SOURCE/config.py $CONFIG_TARGET/config.py
+
