4c7b0e16d2f5492782d39a091131dd2614b9ec02
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-12-05 19:35:22 -0800

    Added the ssh configuration

---
+++ etc/ssh/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# GnuPG Setup Script
+# ------------------
+
+. $HOME/lib/shell/logging && eval "$(get_logger $0)"
+
+logger "Setting up OpenSSH"
+
+CONFIG_SOURCE=$HOME/etc/ssh
+CONFIG_TARGET=$HOME/.ssh
+
+if [ ! -d "$CONFIG_TARGET" ]; then
+    logger "Making directory $CONFIG_TARGET"
+    mkdir $CONFIG_TARGET
+fi
+
+logger "Creating symlink for ssh config file"
+ln -s $CONFIG_SOURCE/config $CONFIG_TARGET/config
