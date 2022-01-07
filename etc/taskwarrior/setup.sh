afda9bdc68798156a747a04192edc7294a1ebd0d
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-01-06 22:37:17 -0800

    taskwarrior setup files

---
+++ etc/taskwarrior/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# GnuPG Setup Script
+# ------------------
+
+. $HOME/lib/shell/logging && eval "$(get_logger $0)"
+
+logger "Setting up taskwarrior"
+
+CONFIG_SOURCE=$HOME/etc/taskwarrior
+CONFIG_TARGET=$HOME
+
+if [ ! -d "$CONFIG_TARGET" ]; then
+    logger "Making directory $CONFIG_TARGET"
+    mkdir $CONFIG_TARGET
+fi
+
+logger "Creating symlink for taskrc"
+ln -s $CONFIG_SOURCE/rc $CONFIG_TARGET/.taskrc
