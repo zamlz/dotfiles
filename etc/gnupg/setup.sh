e597df82555a7d10f20bb2636d60cd4db49d4adc
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-02-03 14:58:24 -0800

    Updated gpg setup script to pull public key

--- etc/gnupg/setup.sh
+++ etc/gnupg/setup.sh
@@ -12,7 +12,7 @@ CONFIG_TARGET=$HOME/.gnupg
 
 if [ ! -d "$CONFIG_TARGET" ]; then
     logger "Making directory $CONFIG_TARGET"
-    mkdir -p $CONFIG_TARGET
+    mkdir -p $CONFIG_TARGET -m 700
 fi
 
 logger "Creating symlink for gpg.conf"
@@ -20,3 +20,8 @@ ln -s $CONFIG_SOURCE/gpg.conf $CONFIG_TARGET/gpg.conf
 
 logger "Creating symlink for gpg-agent.conf"
 ln -s $CONFIG_SOURCE/gpg-agent.conf $CONFIG_TARGET/gpg-agent.conf
+
+logger "Importing public key for zamlz"
+PUBLIC_KEY=$(mktemp)
+curl https://zamlz.org/resources/public.key -o ${PUBLIC_KEY}
+gpg --import ${PUBLIC_KEY}
