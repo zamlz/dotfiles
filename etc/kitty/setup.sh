b39c27c57006762e1c9e6d45971cacb56a66375c
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-02-13 10:49:24 -0800

    Added initial files for kitty terminal

---
+++ etc/kitty/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Picom Setup Script
+# ------------------
+
+. $HOME/lib/shell/logging && eval "$(get_logger $0)"
+
+logger "Setting up Kitty"
+
+CONFIG_SOURCE=$HOME/etc/kitty
+CONFIG_TARGET=$HOME/.config/kitty
+
+if [ ! -d "$CONFIG_TARGET" ]; then
+    logger "Making directory $CONFIG_TARGET"
+    mkdir -p $CONFIG_TARGET
+fi
+
+logger "Creating symlink for kitty.conf"
+ln -s $CONFIG_SOURCE/main.conf $CONFIG_TARGET/kitty.conf
