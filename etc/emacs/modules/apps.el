3342d5ec5d411e8e017a901bbdca9cae92887fc6
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-05-22 11:05:13 -0700

    Added projectile

--- etc/emacs/modules/apps.el
+++ etc/emacs/modules/apps.el
@@ -9,6 +9,30 @@
 (use-package magit-todos
   :after magit
   :config
-  (magit-todos-mode 1))
+  (magit-todos-mode +1))
+
+;; It's also useful to have projectile, a useful project management tool
+(use-package projectile
+  :diminish
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-project-search-path '())
+  (dolist (project-dir '("~/src" "~/usr"))
+    (when (file-directory-p project-dir)
+      (add-to-list 'projectile-project-search-path project-dir)))
+  :config
+  (projectile-mode +1))
+
+;; Helpful is a replacement for the emacs help pages with far more info
+;; and context
+(use-package helpful
+  :bind
+  (("C-h f" . helpful-callable)
+   ("C-h v" . helpful-variable)
+   ("C-h k" . helpful-key)
+   ("C-h F" . helpful-function)
+   ("C-h C" . helpful-command)
+   ("C-h C-d" . helpful-at-point)))
 
 (provide 'apps)
