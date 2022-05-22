4296153b9fe5fc12deea23d2cd1ecbf3d1caa1e6
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-05-21 19:46:22 -0700

    cleaned up editor

--- etc/emacs/modules/editor.el
+++ etc/emacs/modules/editor.el
@@ -2,13 +2,6 @@
 ;; Evil collection expects evil-want-keybinding and evil-want-integration to
 ;; be configured as (nil, t) respectively before both evil and evil-collection
 ;; is initialized.
-(use-package evil
-  :init
-  (setq evil-want-keybinding nil)
-  (setq evil-want-integration t))
-(use-package evil-collection :after evil)
-(use-package evil-commentary :after evil)
-(use-package evil-goggles :after evil)
 
 ;; Here are some saner editor defaults
 (setq show-paren-delay 0)
@@ -19,17 +12,33 @@
 (setq buckup-directory-alist `(("." . "~/.config/emacs/backup")))
 
 ;; Let's make GNU/Emacs more EVIL!!
-(evil-mode 1)
-(evil-commentary-mode 1)
-(evil-collection-init)
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-want-integration t)
+  :config
+  (evil-mode 1))
 
-;; while this is a UI change, this makes emacs "visually" more evil
-(evil-goggles-mode 1)
-(evil-goggles-use-diff-faces)
-
-;; Evil needs to follow some more saner keybindings
+;; TODO: This needs to be converted to a :bind expression somehow for evil above...
 (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
 (define-key evil-normal-state-map (kbd "J") nil)
 (define-key evil-normal-state-map (kbd "K") nil)
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package evil-commentary
+  :after evil
+  :config
+  (evil-commentary-mode 1))
+
+;; while this is a UI change, this makes emacs "visually" more evil
+(use-package evil-goggles
+  :after evil
+  :config
+  (evil-goggles-mode 1)
+  (evil-goggles-use-diff-faces))
 
 (provide 'editor)
