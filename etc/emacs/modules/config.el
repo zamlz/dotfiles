43e372c10d418207b7e89356f83973805292bc7b
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-05-21 20:50:53 -0700

    Integrations across files need to be consolidated here

--- etc/emacs/modules/config.el
+++ etc/emacs/modules/config.el
@@ -2,8 +2,17 @@
 ;; all high level configurations should be defined here.
 ;; Specifically, any custom keybindings and personal settings.
 
+;; Basic user settings
 (setq user-full-name "Amlesh Sivanantham")
 (setq user-mail-address "zamlz@pm.me")
 (setq user-login-name "zamlz")
+
+;; --------------------------------------------------------------
+;; Custom integrations
+;; --------------------------------------------------------------
+
+;; unfortunately, magit's editor doesn't start in "insert" mode which
+;; is really inconvenient. Add this hook to enable that.
+(add-hook 'with-editor-mode-hook #'evil-insert-state)
 
 (provide 'config)
