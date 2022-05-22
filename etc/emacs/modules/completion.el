19daadd2128752c35203e48be4daf45d2620aba6
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-05-21 21:12:03 -0700

    Cleaned up completion file

--- etc/emacs/modules/completion.el
+++ etc/emacs/modules/completion.el
@@ -1,26 +1,35 @@
 
-;; Let's use something lightweight like vertico
-(use-package vertico)
-(use-package orderless)
+;; A very lightweight minibuffer completion system
+(use-package vertico
+  :config
+  (vertico-mode))
+
+;; helps make vertico look nice by annotating completions
+(use-package marginalia
+  :config
+  (marginalia-mode))
+
+;; Cycle between marignalia annotations in vertico
+(define-key vertico-map (kbd "M-m") #'marginalia-cycle)
+
+;; Configure orderless completion
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides '((file (styles partial-completion)))))
+
+
+;; Let's us add icons to the completion annotations
+(use-package all-the-icons-completion
+  :config
+  (all-the-icons-completion-mode))
+
+;; We need to add the icons setup to marginalia's annotations with this hook
+(add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)
+
 (use-package consult)
 (use-package embark)
 (use-package embark-consult)
-(use-package marginalia)
-(use-package all-the-icons-completion)
-
-(vertico-mode)
-(marginalia-mode)
-
-;; Configure orderless completion
-(setq completion-styles '(orderless basic))
-(setq completion-category-defaults nil)
-(setq completion-category-overrides '((file (styles partial-completion))))
-
-;; Cycle between marignalia annotations in vertico
-(define-key vertico-map (kbd "M-A") #'marginalia-cycle)
-
-;; all-the-icons-completion and marginalia need to be combined
-(all-the-icons-completion-mode)
-(add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)
 
 (provide 'completion)
