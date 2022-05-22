93c412d09f80a9ac97f2763c1e58923f15b9ae10
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-05-21 20:49:31 -0700

    Added magit

--- etc/emacs/modules/apps.el
+++ etc/emacs/modules/apps.el
@@ -1,1 +1,8 @@
+;; Here we configure standalone apps for emacs.
+;; some of these are built-in but it's not necessarily the case for all.
+
+;; The single most useful emacs app is =magit=
+;; It provides a very nice interface to work with git repos.
+(use-package magit)
+
 (provide 'apps)
