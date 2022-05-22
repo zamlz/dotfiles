7f90cbc562e81bfb5606dffbeb696fa6317c1d15
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-05-21 20:43:39 -0700

    Cleaned up org mode configuration

--- etc/emacs/modules/zorg.el
+++ etc/emacs/modules/zorg.el
@@ -1,18 +1,21 @@
 ;; Yeah I don't have a good name for this module, but this should
 ;; house everything related to org-mode. (zorg -> zamlz's org-mode)
 
-(use-package org)
-(use-package org-roam :after org)
-
 ;; Shared variables for org modules
 (setq +org-directory (file-truename "~/org"))
 
 ;; setup org
-(setq org-directory +org-directory)
+(use-package org
+  :init
+  (setq org-directory +org-directory))
 
-;; We need to acknowledge org-roam version 2 and enable it
-(setq org-roam-v2-ack t)
-(setq org-roam-directory +org-directory)
-(org-roam-db-autosync-mode)
+;; Let's configure org-roam as well
+(use-package org-roam
+  :after org
+  :init
+  (setq org-roam-v2-ack t)
+  (setq org-roam-directory +org-directory)
+  :config
+  (org-roam-db-autosync-mode))
 
 (provide 'zorg)
