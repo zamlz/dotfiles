6feadf0fdb297b9b94dd4f684ea85161ec557c26
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-09-21 22:11:05 -0700

    no longer need shell script to install vimplug

--- etc/vim/setup.sh
+++ etc/vim/setup.sh
@@ -18,14 +18,3 @@ if [ ! -d "$CONFIG_TARGET_DIR" ]; then
 fi
 
 create_symlink "nvim config" $CONFIG_SOURCE $CONFIG_TARGET
-
-# Installing Vim-Plug
-VIM_PLUG_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-SAVE_PATH=$HOME/.local/share/nvim/site/autoload/plug.vim
-
-if [ ! -f "$SAVE_PATH" ]; then
-    logger "Setting up vim-plug"
-    curl -fLo $SAVE_PATH --create-dirs $VIM_PLUG_URL
-else
-    logger "skipping vim-plug install (already exists)"
-fi
