d5099c20874a66b9b907475b2c0e9d50e6b01628
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2022-10-04 19:31:24 -0700

    updated my lua keybinding configuration

--- etc/vim/runtime/lua/init/keybindings.lua
+++ etc/vim/runtime/lua/init/keybindings.lua
@@ -1,20 +1,50 @@
--- Lua conversion time
-vim.cmd([[
-" Finally, let's remap the leader key (clear any prexisting uses of <SPACE>)
-" FIXME: figure out a better way to utilize my leader keys
-nnoremap <SPACE> <Nop>
-let mapleader=" "
+--[[--------------------------------------------------------------------------
+    __ __           __    _           ___
+   / //_/__  __  __/ /_  (_)___  ____/ (_)___  ____ ______
+  / ,< / _ \/ / / / __ \/ / __ \/ __  / / __ \/ __ `/ ___/
+ / /| /  __/ /_/ / /_/ / / / / / /_/ / / / / / /_/ (__  )
+/_/ |_\___/\__, /_.___/_/_/ /_/\__,_/_/_/ /_/\__, /____/
+          /____/                            /____/
+--------------------------------------------------------------------------]]--
 
-" Sets a keybind to turn off highlighted searches
-nnoremap <leader><space> :nohlsearch<CR>
+-- Keybinding helper functions
 
-" Keybinds to quickly switch buffers
-noremap <S-j> :bn<CR>
-noremap <S-k> :bp<CR>
+function map(mode, shortcut, command)
+  vim.api.nvim_set_keymap(
+    mode,
+    shortcut,
+    command,
+    {noremap=true, silent=true}
+  )
+end
 
+function nmap(shortcut, command)
+  map('n', shortcut, command)
+end
+
+function imap(shortcut, command)
+  map('i', shortcut, command)
+end
+
+-- Finally, let's remap the leader key (clear any prexisting uses of <SPACE>)
+-- FIXME: figure out a better way to utilize my leader keys
+vim.cmd('nnoremap <SPACE> <Nop>')
+vim.cmd('let mapleader=" "')
+
+-- Sets a keybind to turn off highlighted searches
+-- nnoremap <leader><space> :nohlsearch<CR>
+nmap("<leader><space>", ":nohlsearch<CR>")
+
+-- Keybinds to quickly switch buffers
+-- noremap <S-j> :bn<CR>
+nmap("<S-j>", ":bn<CR>")
+-- noremap <S-k> :bp<CR>
+nmap("<S-k>", ":bp<CR>")
+
+-- FIXME: translate this into a meaninful command to use for lua
+--[[
 noremap <F6> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") .
     \ '> trans<' . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
     \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">" .
     \ " FG:" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"fg#")<CR>
-
-]])
+--]]
