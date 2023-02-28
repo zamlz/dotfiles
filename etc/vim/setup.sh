ac8771742d1e27a847e2c16c0e7c3e37223296c8
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2023-02-28 08:58:19 -0800

    Updated config path in vim setup.sh, made idempotent, and linted

--- etc/vim/setup.sh
+++ etc/vim/setup.sh
@@ -3,18 +3,23 @@
 # (Neo)Vim Setup Script
 # ---------------------
 
-. $HOME/lib/shell/logging && eval "$(get_logger $0)"
-. $HOME/lib/shell/utils
+. "$HOME/lib/shell/logging" && eval "$(get_logger "$0")"
+. "$HOME/lib/shell/utils"
 
 logger "Setting up NeoVim"
 
-CONFIG_SOURCE=$HOME/etc/vim/runtime
+CONFIG_SOURCE=$HOME/etc/vim
 CONFIG_TARGET=$HOME/.config/nvim
-CONFIG_TARGET_DIR=$(dirname $CONFIG_TARGET)
+CONFIG_TARGET_DIR=$(dirname "$CONFIG_TARGET")
 
 if [ ! -d "$CONFIG_TARGET_DIR" ]; then
     logger "Making directory $CONFIG_TARGET_DIR"
-    mkdir -p $CONFIG_TARGET_DIR
+    mkdir -p "$CONFIG_TARGET_DIR"
 fi
 
-create_symlink "nvim config" $CONFIG_SOURCE $CONFIG_TARGET
+if [ -h "$CONFIG_TARGET" ]; then
+    logger "Removing existing symlink"
+    unlink "$CONFIG_TARGET"
+fi
+
+create_symlink "nvim config" "$CONFIG_SOURCE" "$CONFIG_TARGET"
