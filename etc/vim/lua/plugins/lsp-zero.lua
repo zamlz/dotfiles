fc6d1783b3a9e463ab7c12caff066a69e85218c2
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2023-07-09 15:18:21 -0700

    New LSP plugin just dropped

---
+++ etc/vim/lua/plugins/lsp-zero.lua
@@ -0,0 +1,115 @@
+-- Basically use LSP Zero to setup my IDE environment
+-- This kinda configures a lot of different plugins
+
+return {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},         -- Required
+        {'hrsh7th/cmp-nvim-lsp'},     -- Required
+        {'hrsh7th/cmp-nvim-lua'},     -- Optional
+        {'hrsh7th/cmp-buffer'},       -- Optional
+        {'hrsh7th/cmp-path'},         -- Optional
+        {'saadparwaiz1/cmp_luasnip'}, -- Optional
+        {'onsails/lspkind.nvim'},     -- Optional
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},             -- Required
+        {'rafamadriz/friendly-snippets'}, -- Optional
+
+        -- Telescope stuff
+        {'nvim-telescope/telescope.nvim'},
+    },
+    config = function()
+        -- GLOBAL LSP ZERO CONFIGURATION
+        local lsp = require('lsp-zero').preset({})
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({
+                buffer = bufnr,
+                omit = {'gl', 'gr', 'K', '[d', ']d', '<F2>', '<F4>'},
+                preserve_mappings = false
+            })
+            -- Custom Telescope LSP functions
+            vim.keymap.set('n', 'gl', '<CMD>lua vim.lsp.buf.hover()<CR>', {buffer = true})
+            vim.keymap.set('n', 'gr', '<CMD>Telescope lsp_references theme=dropdown<CR>', {buffer = true})
+            vim.keymap.set('n', 'gR', '<CMD>lua vim.lsp.buf.rename()<CR>', {buffer = true})
+            vim.keymap.set('n', 'ga', '<CMD>lua vim.lsp.buf.code_action()<CR>', {buffer = true})
+            vim.keymap.set('n', 'gq', '<CMD>lua vim.diagnostic.open_float()<CR>', {buffer = true})
+            vim.keymap.set('n', 'gk', '<CMD>lua vim.diagnostic.goto_prev()<CR>', {buffer = true})
+            vim.keymap.set('n', 'gj', '<CMD>lua vim.diagnostic.goto_next()<CR>', {buffer = true})
+        end)
+        -- set a list of required servers
+        lsp.ensure_installed({
+            'bashls',
+            'clangd',
+            'cssls',
+            'html',
+            'jsonls',
+            'lua_ls',
+            'pyright',
+            'rust_analyzer',
+        })
+        lsp.set_sign_icons({
+            error = '✘',
+            warn = '▲',
+            hint = '⚑',
+            info = '»'
+        })
+        -- required if we want nvim lua to be respected properly by lsp
+        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        lsp.setup()
+
+        -- CMP CONFIG (You need to setup `cmp` after lsp-zero)
+        local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
+        require('luasnip.loaders.from_vscode').lazy_load()
+        cmp.setup({
+            sources = {
+                {name = 'path'},
+                {name = 'nvim_lsp'},
+                {name = 'nvim_lua'},
+                {name = 'buffer', keyword_length = 3},
+                {name = 'luasnip', keyword_length = 2},
+            },
+            mapping = {
+                -- super tab shenanigans
+                ['<Tab>'] = cmp_action.luasnip_supertab(),
+                ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+
+                -- `Enter` key to confirm completion
+                ['<CR>'] = cmp.mapping.confirm({select = true}),
+                ['<C-e>'] = cmp.mapping.abort(),
+
+                -- Ctrl+Space to trigger completion menu
+                ['<C-Space>'] = cmp.mapping.complete(),
+
+                -- Navigate between snippet placeholder
+                ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            formatting = {
+                fields = {'abbr', 'kind', 'menu'},
+                format = require('lspkind').cmp_format({
+                    mode = 'text', -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters
+                    ellipsis_char = '...', -- when popup menu exceed maxwidth replace with ...
+                })
+            }
+        })
+    end
+}
--- etc/vim/lua/plugins/telescope.lua
+++ etc/vim/lua/plugins/telescope.lua
@@ -9,14 +9,13 @@
 
 return {
     'nvim-telescope/telescope.nvim',
+    lazy = false,
     keys = {
         -- FIXME: Configure more of telescope
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
         { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
         { "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
         { "<leader>th", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
-        -- LSP stuff
-        { "<leader>lr", "<cmd>Telescope lsp_references theme=dropdown<cr>", desc = "LSP References" },
     },
     tag = '0.1.0',
     dependencies = {
