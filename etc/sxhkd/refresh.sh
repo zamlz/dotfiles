1086e3ccada853a3d42d649fd5b68aa9f381d86f
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2023-09-04 23:12:28 -0700

    Added more to my herbstluftwm configuration

--- etc/sxhkd/refresh.sh
+++ etc/sxhkd/refresh.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 
-# Need a logger just to keep track of things
-. "$HOME/lib/shell/logging" && eval "$(get_logger "$0")"
-
 # Set some important variables for sxhkd
-SXHKD_CONFIG=$HOME/etc/sxhkd/rc
-SXHKD_LOG=$HOME/.local/share/sxhkd.log
 export SXHKD_SHELL=sh
-export SXHKD_TERMINAL=alacritty
 
 if which sxhkd > /dev/null 2>&1; then
 
     (killall sxhkd \
-        && logger "killed existing sxhkd process") \
-        || logger "No existing sxhkd process found"
+        && echo "killed existing sxhkd process") \
+        || echo "No existing sxhkd process found"
 
     # Wait until the processes have been shut down
     while pgrep -u $UID -x sxhkd >/dev/null; do sleep 1; done
 
-    logger "attempting to start sxhkd"
-    date > "$SXHKD_LOG"
-    sxhkd -c "$SXHKD_CONFIG" >> "$SXHKD_LOG" 2>&1 &
+    echo "attempting to start sxhkd"
+    sxhkd
 
     (sleep 1 && [ "$(pgrep -x sxhkd --count)" -eq 1 ] \
-        && logger "sxhkd started successfully") \
-        || logger "sxhkd has failed to start!"
+        && echo "sxhkd started successfully") \
+        || echo "sxhkd has failed to start!"
 else
-    logger "ERROR: 'sxhkd' is not found!"
+    echo "ERROR: 'sxhkd' is not found!"
 fi
--- home/zamlz.nix
+++ home/zamlz.nix
@@ -142,15 +142,126 @@
 
   # X11 Stuff
 
-  xsession.windowManager.herbstluftwm = {
+  services.sxhkd = let
+    sxhkdTerminal = "xterm";
+  in {
+    enable = true;
+    keybindings = {
+      "super + Return" = "${sxhkdTerminal}";
+    };
+  };
+
+  xsession.windowManager.herbstluftwm =
+  {
     enable = true;
     extraConfig = ''
-    herbstclient emit_hook reload
-    herbstclient keyunbind --all
-    herbstclient unrule --all
-    herbstclient detect_monitors
+      herbstclient emit_hook reload
+      herbstclient keyunbind --all
+      herbstclient unrule --all
+      herbstclient detect_monitors
+      etc/xorg/scripts/refresh_sxhkd.sh
     '';
-    keybinds = {};
+    # mod4 is SUPER
+    # You can use xev to identify X11 keys very easily!
+    keybinds = let
+      super = "Mod4";
+      resizestep = "0.01";
+    in {
+      # FIXME: SXHKD WORKAROUND STUFF
+      "${super}-Ctrl-Alt-Escape" = "quit";
+
+      # Reload WM and Close Window
+      "${super}-Ctrl-Alt-r" = "chain , spawn $HOME/etc/xorg/refresh.sh , reload";
+      "${super}-Ctrl-r" = "reload";
+      "${super}-q" = "close";
+      
+      # Window Focus
+      "${super}-Left" = "focus left";
+      "${super}-Down" = "focus down";
+      "${super}-Up" = "focus up";
+      "${super}-Right" = "focus right";
+      "${super}-h" = "focus left";
+      "${super}-j" = "focus down";
+      "${super}-k" = "focus up";
+      "${super}-l" = "focus right";
+
+      # defining keybindings for cycling the focused client
+      #"${super}-BackSpace" = "cycle_monitor";  # FIXME: this is used already
+      "${super}-Tab" = "cycle +1";
+      "${super}-Shift-Tab" = "cycle_frame +1";
+      "${super}-c" = "cycle_all +1";
+      "${super}-i" = "jumpto urgent";
+
+      # Window Movement
+      "${super}-Shift-Left" = "shift left";
+      "${super}-Shift-Down" = "shift down";
+      "${super}-Shift-Up" = "shift up";
+      "${super}-Shift-Right" = "shift right";
+      "${super}-Shift-h" = "shift left";
+      "${super}-Shift-j" = "shift down";
+      "${super}-Shift-k" = "shift up";
+      "${super}-Shift-l" = "shift right";
+
+      # Splitting Frames
+      "${super}-u" = "split bottom 0.5";
+      "${super}-o" = "split right 0.5";
+      "${super}-Control-space" = "split explode";
+
+      # Resizing Frames
+      "${super}-Control-h" = "resize left +${resizestep}";
+      "${super}-Control-j" = "resize down +${resizestep}";
+      "${super}-Control-k" = "resize up +${resizestep}";
+      "${super}-Control-l" = "resize right +${resizestep}";
+      "${super}-Control-Left" = "resize left +${resizestep}";
+      "${super}-Control-Down" = "resize down +${resizestep}";
+      "${super}-Control-Up" = "resize up +${resizestep}";
+      "${super}-Control-Right" = "resize right +${resizestep}";
+
+      # Workspace Movement
+      "${super}-grave" = "use_previous";
+      "${super}-bracketright" = "use_index +1 --skip-visible";
+      "${super}-bracketleft" = "use_index -1 --skip-visible";
+      "${super}-slash" = "spawn $HOME/etc/herbstluftwm/scripts/tag_utils.sh goto";
+      "${super}-Shift-slash" = "spawn $HOME/etc/herbstluftwm/scripts/tag_utils.sh move";
+      "${super}-BackSpace" = "spawn $HOME/etc/herbstluftwm/scripts/tag_utils.sh remove";
+      
+      # Layout Control
+      "${super}-r" = "remove";
+      "${super}-s" = "floating toggle";
+      "${super}-f" = "fullscreen toggle";
+      "${super}-t" = "pseudotile toggle";
+
+      # The following cycles through the available layouts within a frame, but skips
+      # layouts, if the layout change wouldn't affect the actual window positions.
+      # (I.e. if there are two windows within a frame, the grid layout is skipped.)
+      "${super}-space" = ''
+        or , and . compare tags.focus.curframe_wcount = 2 \
+                 . cycle_layout +1 vertical horizontal max vertical grid \
+           , cycle_layout +1
+      '';
+
+      # Tag Definitions (workspaces)
+      "${super}-1" = "use_index 0";
+      "${super}-2" = "use_index 1";
+      "${super}-3" = "use_index 2";
+      "${super}-4" = "use_index 3";
+      "${super}-5" = "use_index 4";
+      "${super}-6" = "use_index 5";
+      "${super}-7" = "use_index 6";
+      "${super}-8" = "use_index 7";
+      "${super}-9" = "use_index 8";
+      "${super}-0" = "use_index 9";
+      "${super}-Shift-1" = "move_index 0";
+      "${super}-Shift-2" = "move_index 1";
+      "${super}-Shift-3" = "move_index 2";
+      "${super}-Shift-4" = "move_index 3";
+      "${super}-Shift-5" = "move_index 4";
+      "${super}-Shift-6" = "move_index 5";
+      "${super}-Shift-7" = "move_index 6";
+      "${super}-Shift-8" = "move_index 7";
+      "${super}-Shift-9" = "move_index 8";
+      "${super}-Shift-0" = "move_index 9";
+    };
     mousebinds = {};
     rules = [
       "focus=on"
