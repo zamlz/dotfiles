8f586b6db624784f4d91a0e3b278900b58655d0e
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2023-09-15 00:36:28 -0700

    cleared up my tmux configuration

--- home/shell-env.nix
+++ home/shell-env.nix
@@ -94,10 +94,20 @@
 
   programs.tmux = {
     enable = true;
-    prefix = "C-x";
+    prefix = "C-b";
+    mouse = true;
+    historyLimit = 100000;
+    baseIndex = 1;
+    keyMode = "vi";
+    terminal = "screen-256color";
     plugins = with pkgs; [
       tmuxPlugins.pain-control
     ];
+    extraConfig = ''
+      set -g set-titles on
+      set -g set-titles-string '#T'
+      bind r source-file ~/etc/tmux/config \; display "Reloaded Tmux Config"
+    '';
   };
 
   programs.zsh = {
