cba4f04b0a07935c7781369a62e0d8d9c605f5f6
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2023-11-20 22:45:02 -0800

    updated the name of the rofi theme file

--- users/zamlz/xorg/rofi/default.nix
+++ users/zamlz/xorg/rofi/default.nix
@@ -1,10 +1,10 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [];
-  xdg.configFile."rofi/default-theme.rasi".source = ./default-theme.rasi;
+  xdg.configFile."rofi/theme.rasi".source = ./theme.rasi;
   programs.rofi = let
     # FIXME: unclear why this does not work
     # defualt-theme-path = xdg.configHome + xdg.configFile."rofi/default-theme.rasi".target; 
-    default-theme-path = "~/.config/rofi/default-theme.rasi";
+    default-theme-path = "~/.config/rofi/theme.rasi";
   in {
     enable = true;
     theme = default-theme-path; 
