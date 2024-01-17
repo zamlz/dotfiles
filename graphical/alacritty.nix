8a6766c47ff97a776f8fbe9d5199ccd4f0bae734
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-01-16 16:54:28 -0800

    Added modus theme to alacritty

--- graphical/alacritty.nix
+++ graphical/alacritty.nix
@@ -19,7 +19,30 @@
     cyanAlt    = "#8ec07c";
     whiteAlt   = "#ebdbb2";
   };
-  colorScheme = gruvboxBlack;
+  modusVivendi = {
+    background = "#000000";
+    foreground = "#ffffff";
+    black =      "#000000";
+    red =        "#ff5f59";
+    green =      "#44bc44";
+    yellow =     "#d0bc00";
+    blue =       "#2fafff";
+    magenta =    "#feacd0";
+    cyan =       "#00d3d0";
+    white =      "#ffffff";
+    blackAlt =   "#1e1e1e";
+    redAlt =     "#ff5f5f";
+    greenAlt =   "#44df44";
+    yellowAlt =  "#efef00";
+    blueAlt =    "#338fff";
+    magentaAlt = "#ff66ff";
+    cyanAlt =    "#00eff0";
+    whiteAlt =   "#989898";
+  # indexed_colors:
+  #   - { index: 16, color: '0xfec43f' }
+  #   - { index: 17, color: '0xff9580' }
+  };
+  colorScheme = modusVivendi;
 in {
   imports = [];
   programs.alacritty = {
