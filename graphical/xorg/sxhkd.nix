51af1a3997380aad3f7fc0dad3cbd7d2ca551c96
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-01-21 14:58:13 -0800

    Cleaned up sxhkd

--- graphical/xorg/sxhkd.nix
+++ graphical/xorg/sxhkd.nix
@@ -1,18 +1,29 @@
 { inputs, lib, config, pkgs, ... }: let
-  sxhkdTerminal = "alacritty";
+  terminal = "${pkgs.alacritty}/bin/alacritty";
+  rofi = "${pkgs.rofi}/bin/rofi";
 in {
   imports = [];
   services.sxhkd = {
     enable = true;
     keybindings = {
       # Core utils
-      "super + Return" = "${sxhkdTerminal}";
-      "super + e" = "rofi -show run";
-      "super + w" = "rofi -show window";
+      "super + Return" = "${terminal}";
+      "super + e" = "${rofi} -show run";
+      "super + w" = "${rofi} -show window";
       # FIXME: This configuration should somehow be owned by password-store
       "super + p" = "$HOME/.config/rofi/password-store-dmenu.sh";
       "super + shift + p" = "$HOME/.config/rofi/password-store-dmenu.sh --qrcode";
       "super + ctrl + alt + Escape" = "$HOME/.config/rofi/system-manager.sh";
+      # Multimedia and Physical Switches
+      # "XF86MonBrightnessUp" = "";
+      # "XF86MonBrightnessDown" = "";
+      # "XF86AudioMute" = "";
+      # "XF86AudioMicMute" = "";
+      # "XF86AudioRaiseVolume" = "";
+      # "XF86AudioLowerVolume" = "";
+      # "XF86AudioPlay" = "";
+      # "XF86AudioNext" = "";
+      # "XF86AudioPrev" = "";
     };
   };
 }
