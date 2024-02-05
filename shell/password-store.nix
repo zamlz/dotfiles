b1d5972a231826cc06f7297fc47b643a1ec30279
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-02-04 20:12:05 -0800

    Added the update extension for pass

--- shell/password-store.nix
+++ shell/password-store.nix
@@ -1,6 +1,7 @@
 { inputs, lib, config, pkgs, ... }: {
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions (exts: [ exts.pass-update ]);
     settings = {
       PASSWORD_STORE_DIR = "$HOME/sys/passwords";
     };
