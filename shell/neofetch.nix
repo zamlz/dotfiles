0da3729bef1cc558cf39482e3f1809a1df5fff65
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-02-04 19:38:47 -0800

    updated neofetch configuration

--- shell/neofetch.nix
+++ shell/neofetch.nix
@@ -1,4 +1,4 @@
 { inputs, lib, config, pkgs, ... }: {
-  xdg.configFile."neofetch/ascii.txt".source = ./resources/copland.txt;
+  #xdg.configFile."neofetch/ascii.txt".source = ./resources/copland.txt;
   xdg.configFile."neofetch/config.conf".source = ./resources/neofetch.conf;
 }
