df063911a1914f1dd5567a0e387228761ee67776
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-02-22 21:14:13 -0800

    cleaned up window switcher

--- scripts/fzf-window-switcher.sh
+++ scripts/fzf-window-switcher.sh
@@ -3,8 +3,8 @@
 get_window() {
     # this is kinda a hacky way to get rid of my custom prompts if they pop up
     wmctrl -lxp \
-        | sed -e "s/ $(hostname) //g" \
-        | sed -e "/fzf.fzf *Alacritty/d" \
+        | sed -e "/termprompt.termprompt *Alacritty/d" \
+        | awk '{printf "%s ", $1; for(i=6;i<=NF;++i) printf "%s ", $i; print ""}' \
         | fzf --reverse --prompt "Switch Window: "
 }
 
